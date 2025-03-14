42ec7ac3ce01c8e100b448add2cb76730195a20e
ajoke-naje <ajokenakinseye@gmail.com>
2025-03-14 14:05:25 -0400

    Update common config

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main.bundle.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
@@ -18,11 +18,7 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            }      
+            },     
         ],
     },
 };
